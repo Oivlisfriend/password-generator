36e18cc88ace03aaad61b1b0eba2eb57e786fa85
Oivlisfriend <robertomarcelogongo@gmail.com>
2022-09-21 22:21:27 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './assets/css/style.css';
+import formGeraSenha from './modules/formGeraSenha';
+
+formGeraSenha();
---
+++ src/modules/geradores.js
@@ -0,0 +1,18 @@
+const rand = (min, max) => Math.floor(Math.random() * (max - min) + min);
+const geraMaiscula = () => String.fromCharCode(rand(65, 91));
+const geraMinuscula = () => String.fromCharCode(rand(97, 123));
+const geraNumero = () => String.fromCharCode(rand(48, 58));
+const simbolos = ',.;:-_^[]!"#$%&/*|()+={}';
+const geraSimbolo = () => simbolos[rand(0, simbolos.length - 1)];
+
+export default function geraSenha(quantidade, maiusculas, minusculas, numeros, simbolos) {
+    const senhaArray = [];
+    quantidade = Number(quantidade);
+    for (let i = 0; i <= quantidade; i++) {
+        maiusculas && senhaArray.push(geraMaiscula());
+        minusculas && senhaArray.push(geraMinuscula());
+        numeros && senhaArray.push(geraNumero());
+        simbolos && senhaArray.push(geraSimbolo());
+    }
+    return senhaArray.join('').slice(0, quantidade);
+}
